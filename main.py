dde839490c25aea0601d228e00587ea99294f078
Kinza <kinzasheikhsiddiqui@gmail.com>
2025-08-25 20:58:43 +0500

    Import Agent SDK and set up environment variables

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import chromadb
+from dotenv import load_dotenv
+import os
+from agents import Agent, Runner, OpenAIChatCompletionsModel, set_tracing_disabled
+from openai import AsyncOpenAI
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+load_dotenv()
+
+
+# Tracing disabled
+set_tracing_disabled(disabled=True)
+
+# 1. Which LLM Service?
+external_client: AsyncOpenAI = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+# 2. Which LLM Model?
+llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=external_client
+)
 
 chroma_client = chromadb.Client()
 
