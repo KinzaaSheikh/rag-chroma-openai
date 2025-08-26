abfa27587b59385c12b966c67f4d1f0b0d212a24
Kinza <kinzasheikhsiddiqui@gmail.com>
2025-08-26 13:39:02 +0500

    Document read workflow is finally working

--- main.py
+++ main.py
@@ -3,40 +3,44 @@ from dotenv import load_dotenv
 import os
 from agents import Agent, Runner, OpenAIChatCompletionsModel, set_tracing_disabled
 from openai import AsyncOpenAI
+import uuid
 
-gemini_api_key = os.getenv("GEMINI_API_KEY")
-load_dotenv()
+# gemini_api_key = os.getenv("GEMINI_API_KEY")
+# load_dotenv()
 
 
-# Tracing disabled
-set_tracing_disabled(disabled=True)
+# # Tracing disabled
+# set_tracing_disabled(disabled=True)
 
-# 1. Which LLM Service?
-external_client: AsyncOpenAI = AsyncOpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-)
+# # 1. Which LLM Service?
+# external_client: AsyncOpenAI = AsyncOpenAI(
+#     api_key=gemini_api_key,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+# )
 
-# 2. Which LLM Model?
-llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
-    model="gemini-2.5-flash",
-    openai_client=external_client
-)
+# # 2. Which LLM Model?
+# llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
+#     model="gemini-2.5-flash",
+#     openai_client=external_client
+# )
 
-chroma_client = chromadb.Client()
+client = chromadb.Client()
 
-collection = chroma_client.create_collection(name="my_collection")
+collection = client.create_collection(name="exam_info")
+
+with open("clues.txt", "r", encoding="utf-8") as text:
+    content = text.read().splitlines()
 
 collection.upsert(
-    ids=["id1", "id2"],
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ]
+    ids=[str(uuid.uuid4()) for _ in content],
+    documents=content, 
+    metadatas=[{"line": line} for line in range(len(content))]
 )
 
-results = collection.query(
-    query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
-    n_results=2 # how many results to return
-)
-print(results)
+print(collection.peek())
+
+
+# agent = Agent(
+#     name = "Document Query Agent",
+#     instructions="You are a helpful agent that only uses context to answer queries related to learning roadmap"
+# )
