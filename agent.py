8b3b24e5a1d549b8f2e85efb89db766c4df9ae1a
Kinza <kinzasheikhsiddiqui@gmail.com>
2025-08-26 16:04:40 +0500

    Fix model configs

--- agent.py
+++ agent.py
@@ -1,22 +1,30 @@
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
 import os
-from agents import Agent, Runner
+from agents import Agent, Runner, OpenAIChatCompletionsModel, set_tracing_disabled
 import asyncio
+
+set_tracing_disabled(True)
 
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 #Reference: https://ai.google.dev/gemini-api/docs/openai
-# client = AsyncOpenAI(
-#     api_key=gemini_api_key,
-#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-# )
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=client
+)
 
 async def main():
     my_agent = Agent(
     name="Openai Assistant",
-    model="litellm/gemini/gemini-2.5-flash-preview-04-17"
+    model=model, 
+    instructions="You are a helpful assistant"
     )
 
     result = await Runner.run(my_agent, input="What's the capital of France?")
